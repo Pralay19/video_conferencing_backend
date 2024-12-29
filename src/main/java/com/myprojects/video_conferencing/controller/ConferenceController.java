e1837a6c51f3baee50f5f1a64719ab857260cd20
Rajan Khade <rajankhade31@gmail.com>
2024-12-29 11:58:20 +0530

    feat: start recording using egress

--- src/main/java/com/myprojects/video_conferencing/controller/ConferenceController.java
+++ src/main/java/com/myprojects/video_conferencing/controller/ConferenceController.java
@@ -3,8 +3,10 @@ package com.myprojects.video_conferencing.controller;
 import com.google.protobuf.util.JsonFormat;
 import com.myprojects.video_conferencing.dto.RoomCreationRequest;
 import com.myprojects.video_conferencing.helper.MediaServerHelper;
+import io.livekit.server.EgressServiceClient;
 import io.livekit.server.RoomServiceClient;
 import jakarta.annotation.PostConstruct;
+import livekit.LivekitEgress;
 import livekit.LivekitModels;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,6 +33,7 @@ public class ConferenceController {
     private final MediaServerHelper mediaServerHelper;
 
     private RoomServiceClient roomServiceClient;
+    private EgressServiceClient egressServiceClient;
 
     @PostConstruct
     public void init() {
@@ -39,16 +42,22 @@ public class ConferenceController {
                 LivekitKey,
                 LivekitSecret
         );
+
+        this.egressServiceClient = EgressServiceClient.createClient(
+                LivekitHost,
+                LivekitKey,
+                LivekitSecret
+        );
     }
 
     @GetMapping("/conference/media_server/jwt/create_room")
     public ResponseEntity<String> getMediaServerJwt_CreateRoom(@RequestParam String name) {
-        return ResponseEntity.status(HttpStatus.OK).body(mediaServerHelper.createToken_RoomCreation(name, "employee"));
+        return ResponseEntity.status(HttpStatus.OK).body(mediaServerHelper.createToken_RoomCreation(name));
     }
 
     @GetMapping("/conference/media_server/jwt/join_room")
     public ResponseEntity<String> getMediaServerJwt_JoinRoom(@RequestParam String name, @RequestParam String roomId) {
-        return ResponseEntity.status(HttpStatus.OK).body(mediaServerHelper.createToken_RoomJoin(name, "candidate", roomId));
+        return ResponseEntity.status(HttpStatus.OK).body(mediaServerHelper.createToken_RoomJoin(name, roomId));
     }
 
     @PostMapping("/conference/media_server/room/create")
@@ -82,6 +91,25 @@ public class ConferenceController {
             return ResponseEntity.status(HttpStatus.OK).body(roomNames);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/record/start")
+    public ResponseEntity<String> startRecording(@RequestParam String roomName) {
+        try {
+            LivekitEgress.EncodedFileOutput encodedFileOutput = LivekitEgress.EncodedFileOutput.newBuilder()
+                    .setFileType(LivekitEgress.EncodedFileType.MP4)
+                    .setFilepath("/out/"+roomName+".mp4")
+                    .build();
+
+            Call<LivekitEgress.EgressInfo> call = egressServiceClient.startRoomCompositeEgress(roomName, encodedFileOutput);
+            Response<LivekitEgress.EgressInfo> response = call.execute();
+            LivekitEgress.EgressInfo egressInfo = response.body();
+
+            return ResponseEntity.status(HttpStatus.OK).body(egressInfo.toString());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error starting recording");
         }
     }
 }
